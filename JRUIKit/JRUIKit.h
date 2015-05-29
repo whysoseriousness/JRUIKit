c2a68c3137b46a26c8a92ab64d0c989ca2a077a9
JoinUs Development <joinusdevelopment@JoinUss-iMac.local>
2015-05-29 16:08:07 -0400

    Initial Commit

---
+++ JRUIKit/JRUIKit.h
@@ -0,0 +1,19 @@
+//
+//  JRUIKit.h
+//  JRUIKit
+//
+//  Created by JoinUs Development on 5/29/15.
+//  Copyright (c) 2015 Joshua Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JRUIKit.
+FOUNDATION_EXPORT double JRUIKitVersionNumber;
+
+//! Project version string for JRUIKit.
+FOUNDATION_EXPORT const unsigned char JRUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JRUIKit/PublicHeader.h>
+
+
