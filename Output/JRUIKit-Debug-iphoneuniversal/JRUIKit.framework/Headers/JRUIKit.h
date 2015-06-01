17dcdd9402a1d92bd00a1c403bac85fd6dc0d890
JoinUs Development <joinusdevelopment@JoinUss-iMac.local>
2015-06-01 12:15:29 -0400

    working version. steps to make it work atm: 1: build JRUIKit-Universal 2: build JoinUs 3: cp JRUIKit/Output/JRUIKit-Debug-iphoneuniversal/JRUIKit.framework to DerivedData/JoinUs/Build/Products/Debug-iphoneos 4: rerun the product for success

---
+++ Output/JRUIKit-Debug-iphoneuniversal/JRUIKit.framework/Headers/JRUIKit.h
@@ -0,0 +1,21 @@
+//
+//  JRUIKit.h
+//  jm54
+//
+//  Created by Joshua Martin on on 5/29/15.
+//  Copyright (c) 2015 Joshua Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JRUIKit.
+FOUNDATION_EXPORT double JRUIKitVersionNumber;
+
+//! Project version string for JRUIKit.
+FOUNDATION_EXPORT const unsigned char JRUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JRUIKit/PublicHeader.h>
+
+
+#import <JRUIKit/JRButton.h>
+#import <JRUIKit/JRImageView.h>
